427dbd1df8966e67d14ce3a88a1445c3b8e0979f
meiaja <meiaja652@gmail.com>
2026-04-27 10:51:17 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,18 +17,19 @@ let index = 0;
 let currentText = '';
 let letter = '';
 
-function type()
+(function type(){
 
-  if(count === texts.length)
-     count = 0; 
+  if(count === texts.length){ count = 0; }
   currentText = texts[count];
   letter = currentText.slice(0, ++index);
 
   document.getElementById('typing').textContent = letter;
 
-  if(letter.length === currentText.length)
+  if(letter.length === currentText.length){
     count++;
     index = 0;
     setTimeout(type, 1200);
-    else 
+  } else {
     setTimeout(type, 80);
+  }
+})();
